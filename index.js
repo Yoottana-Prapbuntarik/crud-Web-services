adf03f623918db27c3e52b45975bbde7552c68da
unknown <nap3831@gmail.com>
2019-06-30 17:12:12 +0700

    update web-services

--- index.js
+++ index.js
@@ -37,48 +37,62 @@ app.get('/todos', (req, res) => {
 app.post('/todos', (req, res) => {
     let postData = req.body;
     let sql = 'INSERT INTO todos SET ?';
-    db.query(sql, postData, (error, results) => {
-        if (error) throw error;
+    let sql2 = 'INSERT INTO items SET ?';
+
+    db.query(sql, postData, (err, results) => {
+        if (err) throw err;
         res.end(JSON.stringify(results));
     });
+
+    db.query(sql2, postData, (err, results) => {
+        if (err) throw err;
+        res.end(JSON.stringify(results));
+    })
 });
 
 /* UPDATE DATA  */
 app.put('/todos', (req, res) => {
     let sql = 'UPDATE `todos` SET `text`=?,`archive`=? WHERE `id`=? ';
-    db.query(sql, [req.body.text, req.body.archive, req.body.id],(err, results) => {
-        // if (err) throw err; 
+    let sql2 = 'UPDATE `items` SET `text`=?,`archive`=? WHERE `id`=? ';
+    db.query(sql, [req.body.text, req.body.archive, req.body.id], (err, results) => {
+        if (err) throw err;
+        res.end(JSON.stringify(results));
+    })
+    db.query(sql2, [req.body.text, req.body.archive, req.body.id], (err, results) => {
+        if (err) throw err;
         res.end(JSON.stringify(results));
     })
 })
+
+app.put('/todos/edit', (req, res) => {
+    let sql = 'UPDATE `todos` SET `archive`=? WHERE `id`=? ';
+    let sql2 = 'UPDATE `items` SET `archive`=? WHERE `id`=? ';
+    db.query(sql, [req.body.archive, req.body.id], (err, results) => {
+        if (err) throw err;
+        res.end(JSON.stringify(results));
+    })
+    db.query(sql2, [req.body.archive, req.body.id], (err, results) => {
+        if (err) throw err;
+        res.end(JSON.stringify(results));
+    })
+})
+
 
 /* DELETE DATA IN  todos */
 app.delete('/todos', (req, res) => {
     let id = req.body.id;
     console.log(req.body);
     let sql = 'DELETE FROM `todos` WHERE `id`=?';
+    let sql2 = 'DELETE FROM `items` WHERE `id`=?';
     db.query(sql, id, (error, results) => {
         if (error) throw error;
         res.end('Record has been deleted!', results);
     });
+    db.query(sql2, id, (error, results) => {
+        if (error) throw error;
+        res.end('Record has been deleted!', results);
+    });
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
